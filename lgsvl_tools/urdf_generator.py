c52353590a02c54932e91af17c7fc647477b61dc
Masaya Kataoka <ms.kataoka@gmail.com>
2019-11-12 13:22:21 +0900

    configure directory

--- lgsvl_tools/urdf_generator.py
+++ lgsvl_tools/urdf_generator.py
@@ -1,3 +1,25 @@
+#The MIT License (MIT)
+
+#Copyright (c) 2015 bpyamasinn.
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
 import argparse
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 from xml.dom import minidom
@@ -77,7 +99,6 @@ class UrdfGenerator:
         origin.set('xyz', "0 0 0")
         return urdf
     def output_urdf(self,urdf_string):
-        #print(urdf_string)
         with open(self.output_file, mode='w') as f:
             f.write(urdf_string)
         print("urdf saved to "+self.output_file)
